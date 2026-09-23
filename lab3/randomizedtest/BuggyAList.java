f5998a4362289b84cf7f1c5bef14bddd14549c9b
DFAoj6199 <2820084195@qq.com>
2026-09-23 19:57:07 +0800

    randomizeTest finish

--- lab3/randomizedtest/BuggyAList.java
+++ lab3/randomizedtest/BuggyAList.java
@@ -60,7 +60,8 @@ public class BuggyAList<Item> {
       * returns deleted item. */
     public Item removeLast() {
         if ((size < items.length / 4) && (size > 4)) {
-            resize(size / 4);
+            // fix: when size is less than 1/4 of items length, resize to the items.length / 2 instead of size / 4
+            resize(items.length / 2);
         }
         Item x = getLast();
         items[size - 1] = null;
--- lab3/randomizedtest/TestBuggyAList.java
+++ lab3/randomizedtest/TestBuggyAList.java
@@ -9,4 +9,56 @@ import static org.junit.Assert.*;
  */
 public class TestBuggyAList {
   // YOUR TESTS HERE
+    @Test
+    public void testThreeAddThreeRemove() {
+        AListNoResizing<Integer> A = new AListNoResizing<>();
+        BuggyAList<Integer> B = new BuggyAList<>();
+
+        A.addLast(4);
+        B.addLast(4);
+
+        A.addLast(5);
+        B.addLast(5);
+
+        A.addLast(6);
+        B.addLast(6);
+
+        assertEquals(A.removeLast(), B.removeLast());
+        assertEquals(A.removeLast(), B.removeLast());
+        assertEquals(A.removeLast(), B.removeLast());
+    }
+
+    @Test
+    public void randomizedTest() {
+        AListNoResizing<Integer> L = new AListNoResizing<>();
+        BuggyAList<Integer> BL = new BuggyAList<>();
+
+        int N = 5000;
+        for (int i = 0; i < N; i += 1) {
+            int operationNumber = StdRandom.uniform(0, 4);
+            if (operationNumber == 0) {
+                // addLast
+                int randVal = StdRandom.uniform(0, 100);
+                L.addLast(randVal);
+                BL.addLast(randVal);
+                // System.out.println("addLast(" + randVal + ")");
+            } else if (operationNumber == 1) {
+                // getLast
+                if (L.size() > 0) {
+                    // System.out.println("getLast(): " + L.getLast());
+                    assertEquals(L.getLast(), BL.getLast());
+                }
+            } else if (operationNumber == 2) {
+                // removeLast
+                if (L.size() > 0) {
+                    assertEquals(L.removeLast(), BL.removeLast());
+                    // System.out.println("removeLast()");
+                }
+            } else {
+                // System.out.println("size: " + L.size());
+                assertEquals(L.size(), BL.size());
+            }
+        }
+    }
+
 }
